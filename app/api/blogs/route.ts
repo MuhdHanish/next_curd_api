4c00439b14e13c38f28fe35155a890ffdf5a10c1
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-27 16:05:00 +0530

    api/blogs api for getting and posting blogs

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,7 @@
+export async function GET(req: Request, res: Response) {
+    console.log(`GET REQUEST`);
+}
+
+export async function POST(req: Request, res: Response) {
+    console.log(`GET REQUEST`);
+}
