5d500d9ff1e54e498c48513c9483a2efdc6c9af6
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-27 16:05:46 +0530

    api/blogs/id api routes page for getting, updating and deleting post using id

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,11 @@
+export async function GET(req: Request, res: Response) {
+  console.log(`GET REQUEST`);
+}
+
+export async function PUT(req: Request, res: Response) {
+  console.log(`PUT REQUEST`);
+}
+
+export async function DELETE(req: Request, res: Response) {
+  console.log(`DELETE REQUEST`);
+}
